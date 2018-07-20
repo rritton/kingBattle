6962d853b1108bf7c9249a92832c6428ff112bca
norit <bart.daude@gmail.com>
2018-07-20 17:15:07 +0200

    debugage en cours de la fonction moveAutorise

--- jsFiles/gamemove.js
+++ jsFiles/gamemove.js
@@ -2,7 +2,7 @@ var plateau = [ 100 ]; // creation du plateau de 10x10
 var nbRock = 0; // variable du nombre de cases interdites
 var nbArme = 0; // variable du nombre d'armes
 
-// initialisation des case à "libre"
+//initialisation des case à "libre"
 for (var i = 0; i < 10; i++) {
 	for (var j = 0; j < 10; j++) {
 		plateau[i * 10 + j] = 0;
@@ -20,9 +20,9 @@ moveAutorise(50);
  */
 function plateauGenerate() {
 	nbRock = getRandomInt(10, 16);		//génération du nombre de case interdite
-										//entre 10 et 15 
+	//entre 10 et 15 
 	nbArme = getRandomInt(1, 5);		//génération du nombre d'arme
-										//entre 1 et 4
+	//entre 1 et 4
 
 	// Instalation des cases interdites
 	for (var i = 0; i < nbRock; i++) {
@@ -87,33 +87,38 @@ function plateauAffich() {
 	for (var i = 0; i < 10; i++) {				//boucle sur les lignes
 		for (var j = 0; j < 10; j++) {			//boucle sur les colonnes
 			switch (plateau[10 * i + j]) {		//mesure de la valeur associé à la case
-			
+
 			case 0:			//case vide 
 				ecrasement += '<img class="photo colonne' + j + ' ligne' + i
-						+ '" src="../imgFiles/texture.jpg">';
+				+ '" src="../imgFiles/texture.jpg">';
 				break;
 			case 1:			//case interdite
 				ecrasement += '<img class="photo colonne' + j + ' ligne' + i
-						+ '" src="../imgFiles/dark.jpg">';
+				+ '" src="../imgFiles/dark.jpg">';
 				break;
 			case 2:			//case avec une arme
 				ecrasement += '<img class="photo colonne' + j + ' ligne' + i
-						+ '" src="../imgFiles/grenade.jpg">';
+				+ '" src="../imgFiles/grenade.jpg">';
 				break;
 			case 3:			//case du joueur 1
 				ecrasement += '<img class="photo colonne' + j + ' ligne' + i
-						+ '" src="../imgFiles/Brand.png">';
+				+ '" src="../imgFiles/Brand.png">';
 				break;
 			case 5:			//case du joueur 2
 				ecrasement += '<img class="photo colonne' + j + ' ligne' + i
-						+ '" src="../imgFiles/Jinx.png">';
+				+ '" src="../imgFiles/Jinx.png">';
 				break;
 			}
 		}
 		ecrasement += '<br>';			//changement de ligne
 	}
-				//Ecrasement de lancien affichage par le nouveau
-	document.getElementById('zoneDeJeu').innerHTML = ecrasement;
+	//Ecrasement de lancien affichage par le nouveau
+	try{
+		document.getElementById('zoneDeJeu').innerHTML = ecrasement;
+	}
+	catch(e){
+		console.log(e);
+	}
 }
 
 /**
@@ -124,21 +129,49 @@ function plateauAffich() {
  * @returns
  */
 function moveAutorise(position) {
-	for (k = -3; k < 3; k++) {
+	console.log('Bienvenue dans la fonction moveAutorise');
+
+	//gesstion des cases adjacentes horizontalement
+	for (k = -3; k <= 3; k++) {
 		// test des positions aux bords pour ne pas sortir du plateau de jeu
-		if ((position + k) < 0 || (position + k) >= 100
-				|| ((position % 10) < 3 && (position + k) >= 7)
-				|| ((position % 10) >= 7 && (position + k) < 3)) {
+		console.log('Position test : '+(position+k));
+		if (k==0 || (position + k) < 0 || (position + k) >= 100			//sortie au-dessous ou au-dessus (pour premières et dernières cases)
+				|| ((position % 10 < 3) && ((position + k)%10 >= 7))	//sortie à gauche
+				|| ((position % 10 >= 7) && ((position + k)%10 < 3))) {	//sortie à droite
 			continue;
 		}
+
 		//Test si la case est libre ou a une arme (la valeur associée est paire)
+		console.log(plateau[position + k]%2);
 		if (plateau[position + k]%2 == 0 ) {
 			var j = (position + k) % 10;
 			var i = ((position + k) - j) / 10;
 			$(function() {
-				$(' colonne' + j + ' ligne' + i).css('border', 'black').css(
+				$('colonne' + j + ' ligne' + i).css('border', 'black').css(
 						'box-shadow', '0.1em 0.1em 0.1em black');
 			});
+			console.log('('+i+','+j+')');
+		}
+	}
+
+	//gesstion des cases adjacentes verticalement
+	for (k = -3; k <= 3; k++) {
+		// test des positions aux bords pour ne pas sortir du plateau de jeu
+		console.log('Position test : '+(position+10*k));
+		if (k==0 || (position + 10*k) < 0 || (position + 10*k) >= 100) {	//sortie au-dessous ou au-dessus
+			continue;
+		}
+
+		//Test si la case est libre ou a une arme (la valeur associée est paire)
+		console.log(plateau[position + k]%2);
+		if (plateau[position + 10*k]%2 == 0 ) {
+			var j = (position + 10*k) % 10;
+			var i = ((position + 10*k) - j) / 10;
+			$(function() {
+				$('colonne' + j + ' ligne' + i).css('border', 'black').css(
+						'box-shadow', '0.1em 0.1em 0.1em black');
+			});
+			console.log('('+i+','+j+')');
 		}
 	}
 }
