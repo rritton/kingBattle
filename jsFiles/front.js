278811bc3242ec2fb6218a742df544a02a7392a4
norit <bart.daude@gmail.com>
2018-08-02 08:40:53 +0200

    ajout des fichiers de Bertrand

---
+++ jsFiles/front.js
@@ -0,0 +1,17 @@
+$(function(){
+   $('#fight').click(function(){
+      /* $('#epee').css('display', 'inherit');*/
+       /*$('#epee').animate('transform', 'rotate(20deg)');*/
+       /*$('#epee').css('display', 'none');*/
+
+       
+       var test = $('#epee');
+       test.css('display','inherit').animate({tranform : 'rotate(20deg)'});
+      
+   }); 
+});
+
+
+
+
+
